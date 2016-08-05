0ac146cf7fdb2185c5eec1788f7be4db3ae8d040
Grant Livingston <blindrabbits@gmail.com>
2016-08-05 19:47:01 +0100

    Fixes #2:   Converted smoketrial indices from triangle strip to triangles.

--- Assets/Resources/Scripts/Graphics/SmokeTrail.cs
+++ Assets/Resources/Scripts/Graphics/SmokeTrail.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class SmokeTrail : MonoBehaviour {
-	
+
 	// Public classes
 	public class TrailPoint {
 		// Vars
@@ -14,7 +14,7 @@ public class SmokeTrail : MonoBehaviour {
 		public Color Color;
 		public float BackSpeed;
 	}
-	
+
 	// Public vars
 	public float DispersionRate;
 	public float AlphaDecay;
@@ -29,21 +29,21 @@ public class SmokeTrail : MonoBehaviour {
 	public float StartWidth;
 	public bool Enabled {
 		get {
-			return enabled;	
+			return enabled;
 		}
 		set {
 			enabled = value;
 		}
 	}
 	public bool BoostEnabled = false;
-	
+
 	// Private vars
 	MeshFilter filter;
 	LinkedList<TrailPoint> points = new LinkedList<TrailPoint>();
-	
+
 	float cooldown;
 	bool enabled = false;
-	
+
 	Vector3[] vertices;
 	int[] indices;
 	Vector2[] uvs;
@@ -53,7 +53,7 @@ public class SmokeTrail : MonoBehaviour {
 	void Start () {
 		filter = GetComponent<MeshFilter>();
 		filter.mesh = new Mesh();
-		
+
 		// Create the vertices array
 		vertices = new Vector3[MaxVerts];
 		indices = new int[MaxVerts];
@@ -69,7 +69,7 @@ public class SmokeTrail : MonoBehaviour {
 			colors[i] = Color.white;
 		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		// If the follow died, then die.
@@ -82,17 +82,19 @@ public class SmokeTrail : MonoBehaviour {
 			if (cooldown < 0) {
 				// Reset it.
 				cooldown = DropRate;
-				
+
 				// Check if we have a rigid body.
 				Vector2 normal = Vector2.zero;
 				Vector2 outdir = OutDir;
-				if (Follow.GetComponent<Rigidbody>()) {
-					normal = new Vector2(-Follow.GetComponent<Rigidbody>().velocity.z, Follow.GetComponent<Rigidbody>().velocity.x).normalized;
+
+				Rigidbody rigidBody = Follow.GetComponent<Rigidbody>();
+				if (rigidBody) {
+					normal = new Vector2(-rigidBody.velocity.z, rigidBody.velocity.x).normalized;
 				} else {
 					normal = new Vector2(-BackwardsDir.y, BackwardsDir.x).normalized;
 					outdir = BackwardsDir;
 				}
-				
+
 				// Drop down some points.
 				TrailPoint left = new TrailPoint();
 				left.Normal = normal;
@@ -106,7 +108,7 @@ public class SmokeTrail : MonoBehaviour {
 				right.Pos = new Vector2(Follow.position.x, Follow.position.z) - normal * StartWidth;
 				right.Speed = -DispersionRate + Random.Range(-0.5f, 0.5f) * DispersionRate;
 				right.BackSpeed = BackSpeed;// + Follow.rigidbody.velocity.magnitude;
-				
+
 				// Set the color
 				if (BoostEnabled) {
 					left.Color = BoostColor;
@@ -115,23 +117,23 @@ public class SmokeTrail : MonoBehaviour {
 					left.Color = Color;
 					right.Color = Color;
 				}
-				
+
 				// Make them invisible if it is disabled.
 				if (!Enabled) {
 					left.Color *= 0;
 					right.Color *= 0;
 				}
-				
+
 				points.AddLast(right);
 				points.AddLast(left);
 			}
 		}
-		
+
 		// If the point list is over the maximimum.
 		while (points.Count >= MaxVerts) {
 			points.RemoveFirst();
-		}		
-		
+		}
+
 		// Update the points.
 		int count = 0;
 		bool allAlpha = true;
@@ -142,26 +144,36 @@ public class SmokeTrail : MonoBehaviour {
 			vertices[count] = new Vector3(p.Pos.x, 0, p.Pos.y);
 			colors[count] = p.Color;
 			++count;
-			
+
 			if (p.Color.a > 0.01f)
 				allAlpha = false;
 		}
 		if (allAlpha && Follow == null)
 			Destroy(gameObject);
-		
+
 		// Create the mesh.
 		filter.mesh.vertices = vertices;
 		filter.mesh.uv = uvs;
 		filter.mesh.colors = colors;
-		indices = new int[points.Count];
-		for (int i = 0; i < indices.Length; ++i) {
-			indices[i] = i;
-		}		
+
+		// Generate indices
+		indices = new int[(points.Count - 2) * 3];
+		for (int i = 0; i < points.Count - 2; ++i) {
+			if (i % 2 == 0) {
+				indices[i*3 + 0] = i;
+				indices[i*3 + 1] = i + 1;
+				indices[i*3 + 2] = i + 2;
+			} else {
+				indices[i*3 + 0] = i + 2;
+				indices[i*3 + 1] = i + 1;
+				indices[i*3 + 2] = i;
+			}
+		}
 		//filter.mesh.SetTriangleStrip(indices, 0);
 		// Temp
 		filter.mesh.SetTriangles(indices, 0);
 		//filter.mesh.RecalculateNormals();
-		
+
 		// Set the mesh bounds
 		filter.mesh.RecalculateBounds();
 	}
