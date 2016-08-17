2282045b557c2a9ee965e1f120be4f41d09b6dfb
Grant Livingston <blindrabbits@gmail.com>
2016-08-17 18:49:08 +0100

    Using boost now only uses the boost whilst you are pressing the boost button.

--- Assets/Resources/Scripts/Player/PlayerControls.cs
+++ Assets/Resources/Scripts/Player/PlayerControls.cs
@@ -2,18 +2,18 @@ using UnityEngine;
 using System.Collections;
 
 public class PlayerControls : MonoBehaviour {
-	
+
 	// Public vars
 	public float DoubleClickThreshold = 0.2f;
-	
+
 	// Private vars
 	float lastClick = 9999;
-	
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		// If we are dead then don't do anything.
@@ -21,17 +21,14 @@ public class PlayerControls : MonoBehaviour {
 			// Turn off the trail
 			if (GetComponent<AutoSpawnSmokeTrail>().Trail != null) {
 				GetComponent<AutoSpawnSmokeTrail>().Trail.GetComponent<SmokeTrail>().Enabled = false;
-			}			
-			
+			}
+
 			return;
 		}
-		
+
 		// Inc last click
 		lastClick += Time.deltaTime;
-		
-		// Find the world position of the mouse pointer.
-		Vector3 world = transform.position + AnalogueStick.Val;
-		
+
 		// Get the smoke trail and point it.
 		SmokeTrail trail = null;
 		if (GetComponent<AutoSpawnSmokeTrail>().Trail != null) {
@@ -39,12 +36,16 @@ public class PlayerControls : MonoBehaviour {
 			Vector3 outDir = AnalogueStick.Val;
 			trail.OutDir = new Vector2(outDir.x, outDir.z).normalized;
 		}
-		
-		// Control the player 
+
+		// Control the player
 		if (Input.GetMouseButton(0)) {
 			// Move to that point.
-			GetComponent<PlayerMovement>().PropMoveTo(world);
-			
+			if (Input.touchCount >= 2 || Input.GetMouseButton(1)) {
+				GetComponent<PlayerMovement>().Boost(AnalogueStick.Val);
+			} else {
+				GetComponent<PlayerMovement>().Move(AnalogueStick.Val);
+			}
+
 			// Enable the trail and push it in that direction.
 			if (trail != null) {
 				trail.Enabled = true;
@@ -52,21 +53,6 @@ public class PlayerControls : MonoBehaviour {
 		} else {
 			if (trail != null)
 				trail.Enabled = false;
-		}
-		
-		// Check for double clicks
-		if (Input.GetMouseButtonDown(0) && lastClick > DoubleClickThreshold) {
-			lastClick = 0;
-		} else if (Input.GetMouseButtonDown(0) && lastClick < DoubleClickThreshold) {
-			GetComponent<PlayerMovement>().StartBoost();
-		}
-		
-		if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetMouseButtonDown(1)) {
-			GetComponent<PlayerMovement>().StartBoost();	
-		}
-		
-		if (Input.touchCount >= 2) {
-			GetComponent<PlayerMovement>().StartBoost();	
 		}
 	}
 }
--- Assets/Resources/Scripts/Player/PlayerMovement.cs
+++ Assets/Resources/Scripts/Player/PlayerMovement.cs
@@ -2,103 +2,88 @@ using UnityEngine;
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-	
+
 	// Public vars
-	public float Acc;
 	public float MaxSpeed;
 	public float Force = 100.0f;
-	
+
 	public const float BOOST = 3.0f;
-	public const float BOOSTCOOLDOWN = 3.0f;
 	public const float BOOSTSCOREUSE = 1000.0f;
-	
+
 	// Private vars
-	float boostCooldown = 0.0f;
 	bool boosting = false;
-	float boostUsed = 0.0f;
-	
-//	float lastToTurn = 0;		
-	Vector3 oldLOS = Vector3.zero;
-//	float distance = 0;	
-	
-	public void StartBoost() {
-		if (Score.ScoreNum > 0) {
-			boosting = true;
-		}
+
+	// Move to a point.
+	public void Accelerate(Vector3 _dir, float _force) {
+		Vector3 diff = _dir;
+		diff.y = 0;
+		diff.Normalize();
+
+		// Add a force.
+		Debug.Log(GetComponent<Rigidbody>().velocity.magnitude);
+		if (GetComponent<Rigidbody>().velocity.magnitude < MaxSpeed)
+			GetComponent<Rigidbody>().AddForce(diff * _force * Time.deltaTime);
 	}
-	public void StopBoost() {
-		boosting = false;
-	}
-	
+
 	// Use proportional navigation to move to a point.
-	public void PropMoveTo(Vector3 _point, float _force) {
+	Vector3 oldLOS = Vector3.zero;
+	public void PropMoveTo(Vector3 _dir, float _force) {
 
 		// Proportional navigation.
-		Vector3 LOS = _point - transform.position;
+		Vector3 LOS = _dir;
 		Vector3 LOSNormal = new Vector3(LOS.z, 0, -LOS.x);
 		float NC = 20;
-		
+
 		// Calculate the LOSRate
 		Vector3 LOSRate = LOS - oldLOS;
 		oldLOS = LOS;
-		
+
 		// Calculate the APN bias
 		Vector3 APNBias = LOSRate * (NC/2.0f);
-			
+
 		//float APNBias = (NC / 2.0f) * LOS;
 		Vector3 Acc = LOS + (LOSRate * NC);// + APNBias;
-		
+
 		// Move in that direction.
-		if (boosting) {
-			GetComponent<Rigidbody>().AddForce(Acc.normalized * BOOST * _force * Time.deltaTime);			
-		} else {
-			GetComponent<Rigidbody>().AddForce(Acc.normalized * _force * Time.deltaTime);
-		}
-		
-		// Give the trail an update on whether or not we are boosting.
-		if (GetComponent<AutoSpawnSmokeTrail>().Trail != null)
-			GetComponent<AutoSpawnSmokeTrail>().Trail.GetComponent<SmokeTrail>().BoostEnabled = boosting;
+		GetComponent<Rigidbody>().AddForce(Acc.normalized * _force * Time.deltaTime);
 	}
 	public void PropMoveTo(Vector3 _point) {
 		PropMoveTo(_point, Force);
 	}
-	
-	// Move to a point.
-	public void MoveTo(Vector3 _point) {
-		Vector3 diff = _point - transform.position;
-		diff.y = 0;
-		diff.Normalize();
-			
-		// Add a force.
-		GetComponent<Rigidbody>().AddForce((diff).normalized * Force * Time.deltaTime);
-		//rigidbody.AddForce((diff).normalized * Force * Time.deltaTime);
+
+
+	// Shunt in a direction
+	public void Shunt(Vector3 _dir) {
+
 	}
-	
-	public void Boost(Vector3 _dir) {
-		if (Score.ScoreNum > 0) {
+
+	public void Boost(Vector3 _loc) {
+		// Only boost if we can.
+		bool canBoost = Score.ScoreNum > 0;
+
+		// Give the trail an update on whether or not we are boosting.
+		if (GetComponent<AutoSpawnSmokeTrail>().Trail != null)
+			GetComponent<AutoSpawnSmokeTrail>().Trail.GetComponent<SmokeTrail>().BoostEnabled = canBoost;
+
+		if (canBoost) {
 			// Move
-			PropMoveTo(_dir, Force * BOOST);
-			
-			// Reset the cooldown
-			boostCooldown = BOOSTCOOLDOWN;
-			
+			PropMoveTo(_loc, Force * BOOST);
+
 			// Reduce score
 			Score.ScoreNum -= BOOSTSCOREUSE * Time.deltaTime;
 		}
 	}
-	
+
+	public void Move(Vector3 _loc) {
+		// Move without boost.
+		//PropMoveTo(_loc, Force);
+		Accelerate(_loc, Force);
+
+		// Give the trail an update on whether or not we are boosting.
+		if (GetComponent<AutoSpawnSmokeTrail>().Trail != null)
+			GetComponent<AutoSpawnSmokeTrail>().Trail.GetComponent<SmokeTrail>().BoostEnabled = false;
+	}
+
 	public void Update() {
-		boostCooldown -= Time.deltaTime;	
-		
-		// If we are boosting reduce score
-		if (boosting) {
-			// Reduce score
-			Score.ScoreNum -= BOOSTSCOREUSE * Time.deltaTime;
-			boostUsed += BOOSTSCOREUSE * Time.deltaTime;
-			if (boostUsed >= Score.MaxScore / 4 || Score.ScoreNum < 0) {
-				boostUsed = 0.0f;
-				boosting = false;	
-			}
-		}
 	}
 }
